36a11e4a2e6764b83a44b8e6ce6a79994d0c540a
Mara <alaneraerts@live.be>
2021-12-05 20:33:47 +0100

    add:sign in page

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import {HomeIcon, SearchIcon, LibraryIcon, PlusCircleIcon, HeartIcon, RssIcon} from "@heroicons/react/outline";
+import {signOut} from "next-auth/react";
 
 function Sidebar(props) {
     return (
         <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
            <div>
+               <button className="flex items-center space-x-2 hover:text-white"
+                    onClick={()=>signOut()}
+               >
+                   <HomeIcon className="h-5 w-5"/>
+                   <p>Logout</p>
+               </button>
                <button className="flex items-center space-x-2 hover:text-white">
                    <HomeIcon className="h-5 w-5"/>
                    <p>Home</p>
